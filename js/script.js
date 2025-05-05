ad2af3f9fba9f6ba0454ccd06c14cc610aaacfed
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-05-05 00:05:18 +0000

    Volume of a right triangular pyramid

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate() {
+  // input
+  let base = parseFloat(document.getElementById('base-of-triangle').value);
+  let height = parseFloat(document.getElementById('height-of-triangle').value);
+  let pyramidHeight = parseFloat(document.getElementById('height-of-pyramid').value);
+
+  // process
+  let triangleArea = (base * height) * 0.5;
+  let volume = (triangleArea * pyramidHeight) / 3;
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³';
+}
